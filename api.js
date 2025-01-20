f99d83104d59d94864bc13e350a43d2b4680f72a
kouyacha <134472890+ponamaru@users.noreply.github.com>
2025-01-20 09:37:47 +0900

    Update api.js

--- api.js
+++ api.js
@@ -152,7 +152,7 @@ let key = 0;
                 fileReader.onload = ((e)=> {
                     document.getElementById('enemy1').src = e.target.result;
 enemyIc[0] = e.target.result;
-images[0] = e.target.result;
+images[0] = event.target.files;
                 });
                 fileReader.readAsDataURL(obj.files[i]);
             }
@@ -163,7 +163,7 @@ images[0] = e.target.result;
                 fileReader.onload = ((e)=> {
                     document.getElementById('enemy2').src = e.target.result;
 enemyIc[1] = e.target.result;
-images[1] = e.target.result;
+                    images[1] = event.target.files;
                 });
                 fileReader.readAsDataURL(obj.files[i]);
             }
@@ -174,7 +174,7 @@ images[1] = e.target.result;
                 fileReader.onload = ((e)=> {
                     document.getElementById('enemy3').src = e.target.result;
 enemyIc[2] = e.target.result;
-images[2] = e.target.result;
+                    images[2] = event.target.files;
                 });
                 fileReader.readAsDataURL(obj.files[i]);
             }
@@ -184,7 +184,7 @@ images[2] = e.target.result;
                 let fileReader = new FileReader();
                 fileReader.onload = ((e)=> {
                     crI[0] = e.target.result;
-			images[3] = e.target.result;
+			images[3] = event.target.files;
                 });
                 fileReader.readAsDataURL(obj.files[i]);
             }
@@ -193,8 +193,7 @@ images[2] = e.target.result;
             for (i = 0; i < obj.files.length; i++) {
                 let fileReader = new FileReader();
                 fileReader.onload = ((e)=> {
-                    crI[1] = e.target.result;
-			images[4] = e.target.result;
+                    images[4] = event.target.files;
                 });
                 fileReader.readAsDataURL(obj.files[i]);
             }
@@ -203,7 +202,8 @@ images[2] = e.target.result;
             for (i = 0; i < obj.files.length; i++) {
                 let fileReader = new FileReader();
                 fileReader.onload = ((e)=> {
-                    crI[5] = e.target.result;
+                    crI[2] = e.target.result;
+                    images[5] = event.target.files;
                 });
                 fileReader.readAsDataURL(obj.files[i]);
             }
@@ -212,7 +212,8 @@ images[2] = e.target.result;
             for (i = 0; i < obj.files.length; i++) {
                 let fileReader = new FileReader();
                 fileReader.onload = ((e)=> {
-                    crI[6] = e.target.result;
+                    crI[3] = e.target.result;
+                    images[6] = event.target.files;
                 });
                 fileReader.readAsDataURL(obj.files[i]);
             }
@@ -274,7 +275,7 @@ function Cback(obj) {
         document.body.style.backgroundImage = `url(${e.target.result})`;
 
         bgmT[1] = obj.files[0].name;
-	    images[8] = e.target.result;
+	    images[8] = event.target.files;
     };
 
     fileReader.readAsDataURL(obj.files[0]);
