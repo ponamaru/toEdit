12d496b1be421ea326e600fe309a1d92ecdf7098
kouyacha <134472890+ponamaru@users.noreply.github.com>
2025-01-21 16:23:24 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1840,7 +1840,7 @@ imageUrls.forEach((imageUrl, index) => {
   const promise = fetch(imageUrl)
     .then(response => response.blob())
     .then(blob => {
-      const extension = imageUrl.split(',').pop().toLowerCase();
+      const extension = imageUrls.split(',').pop().toLowerCase();
       const imageName = `image.${extension}`; 
 
       folder.file(imageName, blob);
